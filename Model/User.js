5b8d6d1e81922208c48248af70715fdd2962de0b
colbeaf <rimbaud.colin@gmail.com>
2015-04-25 21:43:43 +0200

    BaseObject
    Add BaseObject Class

--- Model/User.js
+++ Model/User.js
@@ -1,11 +1,21 @@
-﻿var User = (function () {
+﻿var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var LibColbeaf = require("../node_modules/libColbeaf/BaseObject");
+
+var User = (function (_super) {
+    __extends(User, _super);
     function User(N, P) {
+        _super.call(this);
         this.N = N;
         this.P = P;
         this.Nom = N;
         this.Prenom = P;
     }
     return User;
-})();
+})(LibColbeaf.BaseObject);
 exports.User = User;
 //# sourceMappingURL=User.js.map
--- Model/User.ts
+++ Model/User.ts
@@ -1,7 +1,10 @@
-﻿export class User {
+﻿import LibColbeaf = require("../node_modules/libColbeaf/BaseObject");
+
+export class User extends LibColbeaf.BaseObject<User> {
     Nom: string;
     Prenom: string;
     constructor(public N, public P) {
+        super();
         this.Nom = N;
         this.Prenom = P;
     }
